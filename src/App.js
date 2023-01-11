977a5c6fc898312a13c3f7b0aa4e585e6995320d
“manetoso” <manetoso98@hotmail.com>
2023-01-10 21:55:50 -0600

    Final CSS changes

--- src/App.js
+++ src/App.js
@@ -30,6 +30,9 @@ const App = () => {
         alignItems: 'center',
       }}>
       <section style={{ height: '80vh', width: '80vw' }}>
+        <p>Choose the item you want to resize/drag to focus them and do it</p>
+        <p>To create a new item click on the button on the bototm right.</p>
+        <p>If you want to delete it, press double click on it</p>
         <div
           id='parent'
           style={{
@@ -49,11 +52,13 @@ const App = () => {
             />
           ))}
         </div>
-        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
           {images.length === 0 ? (
             <p>Loading Images..</p>
           ) : (
-            <button className='button' onClick={addMoveable}>Add Item</button>
+            <button className='button' onClick={addMoveable}>
+              Add Item
+            </button>
           )}
         </div>
       </section>
